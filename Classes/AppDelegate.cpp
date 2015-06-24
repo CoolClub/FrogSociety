d186a6510d928397477e3d03e46162643362b260
Kelvin Jin <kkj9@cornell.edu>
2015-06-23 23:47:27 -0700

    added project files. one more build error left

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -1,5 +1,5 @@
-#include "BoPAppDelegate.h"
-#include "BoPGameController.h"
+#include "AppDelegate.h"
+#include "GameController.h"
 
 USING_NS_CC;
 
@@ -50,7 +50,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
 	auto scene = Scene::create();
 
 	// 'layer' is an autorelease object
-	auto layer = BoPGameController::create();
+	auto layer = GameController::create();
 
 	// add layer as a child to scene
 	scene->addChild(layer);
